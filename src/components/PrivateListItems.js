3fd5db5370b09d6c385edb9df244666ff9d9c897
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2019-01-17 22:11:26 +0000

    Update

--- src/components/PrivateListItems.js
+++ src/components/PrivateListItems.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const PrivateListItems = () => (
-  <>
+  <Fragment>
     <li className="list-group-item">Private Item 1</li>
     <li className="list-group-item">Private Item 2</li>
     <li className="list-group-item">Private Item 3</li>
-  </>
+  </Fragment>
 );
 
 export default PrivateListItems;
--- src/components/PublicListItems.js
+++ src/components/PublicListItems.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 const PublicListItems = () => (
-  <Fragment>
+  <>
     <li className="list-group-item">Public Item 1</li>
     <li className="list-group-item">Public Item 2</li>
     <li className="list-group-item">Public Item 3</li>
-  </Fragment>
+  </>
 );
 
 export default PublicListItems;
